cd5b7714d9e7684ed15b740033560deb6375092e
Elias Nacher <elias.nacher@altia.es>
2024-04-26 13:17:10 +0200

    Add bottom bar

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,6 +22,15 @@ PlugPackages({
             'nvim-lua/plenary.nvim',
             'neovim/nvim-lspconfig'
         }
+    },
+    {
+	'catppuccin/nvim'
+    },
+    {
+	'vim-airline/vim-airline',
+	{
+		'vim-airline/vim-airline-themes'
+	}
     }
 })
 
@@ -45,7 +54,7 @@ require('nvim-autopairs').setup({
 
 require('phpactor').setup({
     install = {
-        bin = "phpactor",
+        bin = vim.fn.expand("$HOME/.config/composer/vendor/bin/phpactor"),
         php_bin = "php",
         composer_bin = "composer",
         git_bin = "git",
@@ -57,6 +66,59 @@ require('phpactor').setup({
         mode = "full"
     },
 })
+
+require("catppuccin").setup({
+    flavour = "auto", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+    },
+    transparent_background = false, -- disables setting the background color.
+    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
+    no_italic = false, -- Force no italic
+    no_bold = false, -- Force no bold
+    no_underline = false, -- Force no underline
+    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    default_integrations = true,
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+            enabled = true,
+            indentscope_color = "",
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+})
+
+-- setup must be called before loading
+vim.cmd.colorscheme 'catppuccin'
+vim.g.airline_theme = 'google_dark'
 
 kb = KeyBind
 
